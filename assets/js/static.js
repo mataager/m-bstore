5a188c6c3e1f5c7de8f65df3833b20f619312b35
mataager <omarbussmo077@gmail.com>
2024-11-07 13:43:54 +0200

    Add files via upload

---
+++ assets/js/static.js
@@ -0,0 +1,50 @@
+const url = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const storeurl = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const storename = "M&B";
+const uid = "qHpNGAepeeSA4WjcQDZ0UwC8Usy2";
+const storePhoneNum = "+20 122 876 6117";
+const storePhoneNum2 = "";
+const storeEmail = "mbwomenstore@gmail.com";
+const storeGender = ["Women"];
+const storeType = "clothing"; // clothing or footwear
+const freeshipping = "2000";
+//images url
+const pannerurl = "https://i.imgur.com/rOhAGC7.jpeg";
+//
+const mentopurl = "";
+const menbottomurl = "";
+const menfootwareurl = "";
+const menshoseseurl = "";
+const menslippersurl = "";
+const menflipflopsurl = "";
+const mensandalsurl = "";
+const menLoafersurl = "";
+const menMoccasinsurl = "";
+const menclassicshoseurl = "";
+const menbootsurl = "";
+//
+const womentopurl = "";
+const womenbottomurl = "";
+const womenfootwareurl = "";
+const womenshoseseurl = "";
+const womenheelsurl = "";
+const womenslippersurl = "";
+const womenflipflopsurl = "";
+const womensandalsurl = "";
+const womenLoafersurl = "";
+const womenMoccasinsurl = "";
+const womenbootsurl = "";
+//
+const kidtopurl = "";
+const kidbottomurl = "";
+const kidfootwareurl = "";
+const kidshoseseurl = "";
+const kidslippersurl = "";
+const kidsandalsurl = "";
+const kidbootssurl = "";
+//
+const menscollectionurl = "";
+const womencollectionurl = "https://i.imgur.com/29qu7qr.jpeg";
+const kidscollectionurl = "https://i.imgur.com/JD0zRaH.jpeg";
+const newarrivalurl = "https://i.imgur.com/TyhOTkh.jpeg";
+const saleurl = "https://i.imgur.com/In5bv7c.jpeg";
